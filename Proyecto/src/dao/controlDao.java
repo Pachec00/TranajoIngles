1c2c5c0519340755f70ff4f5bfbb72ec587aeae7
fabio <fabio>
2023-11-07 20:43:56 +0100

    .

---
+++ Proyecto/src/dao/controlDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class controlDao {
+
+	public Integer consultarControlDao(Connection conn) throws SQLException {
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			String sql = "select * from control";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			Integer control = 0;
+			if(rs.next()) {
+				control = rs.getInt("Numero");
+			}
+			
+			return control;
+		} finally {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+				
+			}
+		}
+	}
+	
+	public void controlDao(Connection conn) throws SQLException {
+		PreparedStatement stmt = null;
+		
+		try {
+			
+			String sql = "update control set Numero = ?";
+			stmt = conn.prepareStatement(sql);
+			Integer control = consultarControlDao(conn);
+			control++;
+			stmt.setInt(1, control);
+			stmt.execute();
+		} finally {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+				
+			}
+		}
+	}
+}
---
+++ Proyecto/src/service/ControlService.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import dao.controlDao;
+
+public class ControlService {
+
+	private OpenConnection openConnection;
+	
+	public ControlService() {
+		openConnection = new OpenConnection();
+	}
+	
+	public void controlService() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			
+			conn = openConnection.getConnection();
+			controlDao c = new controlDao();
+			c.controlDao(conn);
+		} finally {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				
+			}
+		}
+	}
+}
