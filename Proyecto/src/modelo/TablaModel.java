17a9597e72c23ed48562bc495aabb968d36a0d5d
pablo <paretosfas@gmail.com>
2023-11-16 16:11:34 +0100

    as

---
+++ Proyecto/src/modelo/TablaModel.java
@@ -0,0 +1,59 @@
+package modelo;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+
+
+public class TablaModel extends AbstractTableModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9091737343105705648L;
+	private List<Jugador> lista;
+
+	public TablaModel() {
+
+		lista = new ArrayList<>();
+	}
+
+	public int getRowCount() {
+		return lista.size();
+	}
+
+	public int getColumnCount() {
+		return 2;
+	}
+
+	public String getColumnName(int column) {
+		if (column == 0) {
+			return "Nombre";
+		} else{
+			return "Puntuacion";
+		} 
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Jugador jugador = lista.get(rowIndex);
+		if (columnIndex == 0) {
+			String marca = jugador.getNombre();
+			return marca;
+
+		} else   {
+			return jugador.getPuntuacion();
+		} 
+
+	}
+
+	public List<Jugador> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Jugador> lista) {
+		this.lista = lista;
+	}
+
+}
